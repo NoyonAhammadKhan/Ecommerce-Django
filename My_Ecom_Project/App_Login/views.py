64c4e4b5d086c1dab63e47f8b26c6f56261b6369
NoyonAhammadKhan <noyonahammadkhan6464@gmail.com>
2021-07-22 07:38:54 +0800

    after message setup

--- My_Ecom_Project/App_Login/views.py
+++ My_Ecom_Project/App_Login/views.py
@@ -10,6 +10,9 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, logout, authenticate
 
+#import messages
+from django.contrib import messages
+
 
 def sign_up(request):
     form = SignupForm()
@@ -17,6 +20,7 @@ def sign_up(request):
         form = SignupForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, "Account Created Successfully")
             return HttpResponseRedirect(reverse('App_Login:login'))
     return render(request, 'App_Login/sign_up.html', context={'form': form})
 
@@ -37,4 +41,20 @@ def login_user(request):
 @login_required
 def logout_user(request):
     logout(request)
+    messages.warning(request,"You are logged out")
     return HttpResponse("Logged Out")
+
+
+@login_required
+def user_profile(request):
+    profile = Profile.objects.get(user=request.user)
+    form = ProfileForm(instance=profile)
+    if request.method =="POST":
+        form = ProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request,'Changed Saved')
+            form = ProfileForm(instance=profile)
+    return render(request, 'App_Login/change_profile.html', context={'form': form})
+
+
--- My_Ecom_Project/My_Ecom_Project/settings.py
+++ My_Ecom_Project/My_Ecom_Project/settings.py
@@ -124,7 +124,7 @@ STATICFILES_DIR =[STATIC_DIR]
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
 
-LOGIN_URL = '/'
+LOGIN_URL = '/account/login/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
