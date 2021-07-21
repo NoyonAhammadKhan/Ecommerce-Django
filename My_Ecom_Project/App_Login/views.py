0e1f9d6ad582b45581113a9ab1b6e275b0f82fd6
NoyonAhammadKhan <noyonahammadkhan6464@gmail.com>
2021-07-21 20:58:31 +0800

    User Signup

--- My_Ecom_Project/App_Login/views.py
+++ My_Ecom_Project/App_Login/views.py
@@ -17,7 +17,7 @@ def sign_up(request):
         form = SignupForm(request.POST)
         if form.is_valid():
             form.save()
-            return HttpResponseRedirect(reverse())
+            return HttpResponseRedirect(reverse('App_Login:login'))
     return render(request, 'App_Login/sign_up.html', context={'form': form})
 
 def login_user(request):
@@ -32,3 +32,9 @@ def login_user(request):
                 login(request, user)
                 return HttpResponse('logged in')
     return render(request, 'App_Login/login.html', context={'form': form})
+
+
+@login_required
+def logout_user(request):
+    logout(request)
+    return HttpResponse("Logged Out")
