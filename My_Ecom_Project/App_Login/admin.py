78f6825914666aa323399870fa59097cf917ac5f
NoyonAhammadKhan <noyonahammadkhan6464@gmail.com>
2021-07-21 18:53:01 +0800

    commit after making first migrations

---
+++ My_Ecom_Project/App_Login/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from App_Login.models import UserProfile, Profile
+# Register your models here.
+
+
+admin.site.register(UserProfile)
+admin.site.register(Profile)
--- README.md
+++
@@ -1,2 +0,0 @@
-# Ecommerce-Django
-This project is about ecommerce with Django with payment gateway.
