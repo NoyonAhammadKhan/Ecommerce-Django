a4a248680cb1cf2959b1d544c1b49de26aca05a1
NoyonAhammadKhan <noyonahammadkhan6464@gmail.com>
2021-07-22 10:02:49 +0800

    Home Page And Navbar

--- My_Ecom_Project/App_Login/urls.py
+++ My_Ecom_Project/App_Login/urls.py
@@ -8,5 +8,5 @@ urlpatterns =[
      path('signup/',views.sign_up, name='signup'),
      path('login/',views.login_user, name='login'),
      path('logout/', views.logout_user, name='logout'),
-     path('profile/', views.user_profile, name='profile')
+     path('profile/', views.user_profile, name='profile'),
 ]
