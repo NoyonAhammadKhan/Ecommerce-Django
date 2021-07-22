881ecaa19b006e72af1bdbd3936df5249213a10c
NoyonAhammadKhan <noyonahammadkhan6464@gmail.com>
2021-07-22 08:12:54 +0800

    Product Model

--- My_Ecom_Project/App_Login/views.py
+++ My_Ecom_Project/App_Login/views.py
@@ -34,7 +34,7 @@ def login_user(request):
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
-                return HttpResponse('logged in')
+                return HttpResponseRedirect(reverse('App_Shop:home'))
     return render(request, 'App_Login/login.html', context={'form': form})
 
 
@@ -42,7 +42,7 @@ def login_user(request):
 def logout_user(request):
     logout(request)
     messages.warning(request,"You are logged out")
-    return HttpResponse("Logged Out")
+    return HttpResponseRedirect(reverse('App_Shop:home'))
 
 
 @login_required
--- My_Ecom_Project/App_Shop/views.py
+++ My_Ecom_Project/App_Shop/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+#Import Django  Views
+
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
+
+from App_Shop.models import Product
+
+class Home(ListView):
+    model = Product
+    template_name = 'App_Shop/home.html'
